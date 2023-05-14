502ca13539ee4b1d5b7e8e4959744fdcbd9f9748
aisumegane <83156031+aisumegane@users.noreply.github.com>
2023-05-14 13:39:19 +0900

    #add：初期ファイルの追加

---
+++ 230514_Keyboard_Controller.ino
@@ -0,0 +1,129 @@
+/*
+  KeyboardAndMouseControl
+
+  Controls the mouse from five pushbuttons on an Arduino Leonardo, Micro or Due.
+
+  Hardware:
+  - five pushbuttons attached to D2, D3, D4, D5, D6
+
+  The mouse movement is always relative. This sketch reads four pushbuttons, and
+  uses them to set the movement of the mouse.
+
+  WARNING: When you use the Mouse.move() command, the Arduino takes over your
+  mouse! Make sure you have control before you use the mouse commands.
+
+  created 15 Mar 2012
+  modified 27 Mar 2012
+  by Tom Igoe
+
+  This example code is in the public domain.
+
+  https://www.arduino.cc/en/Tutorial/BuiltInExamples/KeyboardAndMouseControl
+*/
+
+#include "Keyboard.h"
+#include "Mouse.h"
+
+// set pin numbers for the five buttons:
+const int upButton = 2;
+const int downButton = 3;
+const int leftButton = 4;
+const int rightButton = 5;
+const int mouseButton = 6;
+
+void setup() {  // initialize the buttons' inputs:
+  pinMode(upButton, INPUT);
+  pinMode(downButton, INPUT);
+  pinMode(leftButton, INPUT);
+  pinMode(rightButton, INPUT);
+  pinMode(mouseButton, INPUT);
+
+  Serial.begin(9600);
+  // initialize mouse control:
+  Mouse.begin();
+  Keyboard.begin();
+}
+
+void loop() {
+  // use serial input to control the mouse:
+  if (Serial.available() > 0) {
+    char inChar = Serial.read();
+
+    switch (inChar) {
+      case 'u':
+        // move mouse up
+        Mouse.move(0, -40);
+        break;
+      case 'd':
+        // move mouse down
+        Mouse.move(0, 40);
+        break;
+      case 'l':
+        // move mouse left
+        Mouse.move(-40, 0);
+        break;
+      case 'r':
+        // move mouse right
+        Mouse.move(40, 0);
+        break;
+      case 'm':
+        // perform mouse left click
+        Mouse.click(MOUSE_LEFT);
+        break;
+    }
+  }
+
+  // use the pushbuttons to control the keyboard:
+  //
+  /*
+  if (digitalRead(upButton) == HIGH) {
+    Keyboard.press('w');
+  }
+  if (digitalRead(upButton) == LOW){
+    Keyboard.release('w');
+  }
+  */
+
+  
+
+  
+  
+  static int count_now = 0;
+  static int count_now_2 = 0;
+
+  if(digitalRead(upButton) == HIGH){
+    if(count_now<2){
+      Mouse.move(0,0,1);
+    }
+    count_now++;
+    if(count_now == 2){
+      count_now = 2;                    //count_nowオーバーフロー時の誤動作防止
+    }
+  }else if(digitalRead(upButton)==LOW){
+    count_now=0;
+  }
+  
+  if(digitalRead(downButton) == HIGH){
+    if(count_now_2<7){
+      Mouse.move(0,0,1);
+    }
+    count_now_2++;
+  }else if(digitalRead(downButton)==LOW){
+    count_now_2=0;
+  }  
+
+/*
+  if (digitalRead(downButton) == HIGH) {
+    Keyboard.write('d');
+  }
+  if (digitalRead(leftButton) == HIGH) {
+    Keyboard.write('l');
+  }
+  if (digitalRead(rightButton) == HIGH) {
+    Keyboard.write('r');
+  }
+  if (digitalRead(mouseButton) == HIGH) {
+    Keyboard.write('m');
+  }
+  */
+}
